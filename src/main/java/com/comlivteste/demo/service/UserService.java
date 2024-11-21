07a55352cf73e359f231f60178e98f515571be86
Vanessa lins <69607861+Vanessa867@users.noreply.github.com>
2024-11-21 20:38:57 +0000

    errado

--- src/main/java/com/comlivteste/demo/service/UserService.java
+++ src/main/java/com/comlivteste/demo/service/UserService.java
@@ -1,5 +1,31 @@
 package com.comlivteste.demo.service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.comlivteste.demo.model.UserModel;
+import com.comlivteste.demo.repository.IUserRepository;
+
+@Service
 public class UserService {
     
+    @Autowired
+    private IUserRepository userRepository;
+
+    public UserModel insertUser(UserModel user){
+        userRepository.save(user);
+        return user;
+    }
+
+    public UserModel findById(Long id){
+        Optional<UserModel> resultUser = userRepository.findById(id);
+
+        if (resultUser.isPresent()) {
+            return resultUser.get();
+        }
+        return null;
+    }
+
 }
